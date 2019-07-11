dcac6c8b4e97882190cae5a4cd4e118bca837dd4
vanufanofsk <b.vanmathi97@gmail.com>
2019-07-11 15:36:11 +0530

    second task

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -12,13 +12,16 @@ public class Main
     public static void main( String[] args )
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        System.out.println("Success");
-        Movie movie=context.getBean("movie",Movie.class);
-       movie.display();
-        Resource res= new ClassPathResource("beans.xml");
-        BeanFactory factory=new XmlBeanFactory(res);
-        Movie movie1=factory.getBean("movie",Movie.class);
-        movie1.display();
+        Movie movie1=context.getBean("movie1",Movie.class);
+        Movie movie1ref=context.getBean("movie1",Movie.class);
+       movie1.display();
+       Movie movie2=context.getBean("movie2",Movie.class);
+        Movie movie2ref=context.getBean("movie2",Movie.class);
+        movie2.display();
+        Movie movie3=context.getBean("movie3",Movie.class);
+        movie3.display();
+        System.out.println("Singleton scope for movie1 "+ (movie1==movie1ref));
+        System.out.println("Prototype scope for movie2 "+ (movie2==movie2ref));
 
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -3,7 +3,7 @@ package com.stackroute.domain;
 public class Movie {
     Actor actor;
 
-    public void setActor(Actor actor) {
+    public Movie(Actor actor) {
         this.actor = actor;
     }
 
