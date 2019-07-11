8577df516235337bfbc9258bd6901b3547a41dd0
vanufanofsk <b.vanmathi97@gmail.com>
2019-07-11 15:58:10 +0530

    second task

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,27 +1,30 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 public class Main
 {
     public static void main( String[] args )
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        //Singleton movie1
         Movie movie1=context.getBean("movie1",Movie.class);
         Movie movie1ref=context.getBean("movie1",Movie.class);
        movie1.display();
+       //prototype movie2
        Movie movie2=context.getBean("movie2",Movie.class);
         Movie movie2ref=context.getBean("movie2",Movie.class);
         movie2.display();
+
         Movie movie3=context.getBean("movie3",Movie.class);
         movie3.display();
+        //Rename <bean name="movieA,movieB"...>
+        Movie moviename=context.getBean("movieA",Movie.class);
+        Movie movienameref=context.getBean("movieB",Movie.class);
         System.out.println("Singleton scope for movie1 "+ (movie1==movie1ref));
         System.out.println("Prototype scope for movie2 "+ (movie2==movie2ref));
+        System.out.println("Replace id with name for two movies "+(moviename==movienameref));
 
     }
 }
